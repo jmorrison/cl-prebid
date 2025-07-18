8eb366ec8c27176e65a63c993c9492c9e5c101ed
John Morrison <jm@symbolic-simulation.com>
2025-07-18 17:11:12 -0400

    Checkpoint.  Native ads still do not work.  But banner ones do.  Now going to change from literal Javascript to Parenscript.

--- easy-handlers.lisp
+++ easy-handlers.lisp
@@ -99,18 +99,306 @@
      (:head
       (:title "Landing Page")
       (:base :href "/")
-      (:script (str (cl-prebid::slurp-javascript-file "Prebid.js/build/dev/prebid.js")))
+      (:script (str (cl-prebid::slurp-javascript-file "prebid10.2.0.js")))
       )
      (:body
       (:div
-       :id="div-0" :style "min-height:250px"
+       :id "div-0" :style "min-height:250px"
        (:p (:a "landing page div-0"))
-       (:p (:a "foo 1: " cl-prebid::*foo*))
-       (:p (:a "foo 2: " (str cl-prebid::*foo*)))
-       #+NIL (:p (:a "prebid-js 1: " cl-prebid::*prebid-js*))
-       #+NIL (:p (:a "prebid-js 2: " (str cl-prebid::*prebid-js*)))
+       (:p (:a :href "./test-bidder-banner-example" (:b "testBidderBannerExample")))
+       (:p (:a :href "./test-bidder-native-example" (:b "testBidderNativeExample")))
+       (:p (:a :href "./test-bidder-video-example" (:b "testBidderVideoExample")))
        )))))
       
+(define-easy-handler (test-bidder-banner-example :uri "/test-bidder-banner-example" :acceptor-names '(normal-acceptor))
+    ()
+  "testBidderBannerExample"
+  (with-html-output-to-string (*standard-output* nil :prologue t)
+    (:html
+     (:head
+      (:title "testBidderBannerExample")
+      ;; (:script (str (cl-prebid::slurp-javascript-file "Prebid.js/build/dist/prebid.js")))
+      (:script (str (cl-prebid::slurp-javascript-file "prebid10.2.0.js")))
+      )
+     (:body
+      (:div
+       :id "banner")
+      (:div
+       (:p (:a "pa1"))
+       (:p (:a "pa2")))
+      (:script
+       "
+            var pbjs = pbjs || {};
+            pbjs.que = pbjs.que || [];
+
+            const adUnitCode = 'adUnit-0000';
+
+            const adUnits = [{
+                mediaTypes: {
+                    banner: {
+                        sizes: [600, 500]
+                    }
+                },
+                code: adUnitCode,
+                bids: [
+                    {bidder: 'testBidder', params: {}}
+                ]
+            }]
+
+            pbjs.que.push(function () {
+
+                /**
+                 * BID RESPONSE SIMULATION SECTION START
+                 *
+                 * This section handles simulating a bidder
+                 * that will always respond with bid responses.
+                 *
+                 * This part should not be present in production.
+                 */
+                pbjs.registerBidAdapter(null, 'testBidder', {
+                    supportedMediaTypes: ['banner', 'video', 'native'],
+                    isBidRequestValid: () => true
+                });
+
+                pbjs.setConfig({
+                    debugging: {
+                        enabled: true,
+                        intercept: [
+                            {
+                                when: {
+                                    bidder: 'testBidder',
+                                },
+                                then: {
+                                    creativeId: 'testCreativeId',
+                                }
+                            }
+                        ]
+                    }
+                });
+                /**
+                 * BID RESPONSE SIMULATION SECTION END
+                 */
+
+                pbjs.addAdUnits(adUnits);
+                pbjs.requestBids({
+                    adUnitCodes: [adUnitCode],
+                    bidsBackHandler: function() {
+                        const bids = pbjs.getHighestCpmBids(adUnitCode);
+                        const winningBid = bids[0];
+                        const div = document.getElementById('banner');
+                        let iframe = document.createElement('iframe');
+                        iframe.frameBorder = '0';
+                        div.appendChild(iframe);
+                        var iframeDoc = iframe.contentWindow.document;
+                        pbjs.renderAd(iframeDoc, winningBid.adId);
+                    }
+                });
+            });
+")))
+    )
+  )
+
+(define-easy-handler (test-bidder-native-example :uri "/test-bidder-native-example" :acceptor-names '(normal-acceptor))
+    ()
+  "testNativeBannerExample"
+  (with-html-output-to-string (*standard-output* nil :prologue t)
+    (:html
+     (:head
+      (:title "testNativeBannerExample")
+      ;; (:script (str (cl-prebid::slurp-javascript-file "Prebid.js/build/dist/prebid.js")))
+      (:script (str (cl-prebid::slurp-javascript-file "prebid10.2.0.js")))
+      (:script
+       "
+            var pbjs = pbjs || {};
+            pbjs.que = pbjs.que || [];
+
+            const adUnitCode = 'adUnit-0000';
+
+            const adUnits = [{
+                mediaTypes: {
+                    native: {
+                        ortb: {
+                            assets: [
+                                { 
+                                    required: true,
+                                    id: 1,
+                                    img: {
+                                        type: 3,
+                                        wmin: 100,
+                                        hmin: 100,
+                                    }
+                                },
+                                {
+                                    required: true,
+                                    id: 2,
+                                    img: {
+                                        type: 3,
+                                        wmin: 200,
+                                        hmin: 200
+                                    }
+                                },
+                                {
+                                    required: true,
+                                    id: 3,
+                                    data: {
+                                        type: 3,
+                                        len: 20
+                                    }
+                                },
+                                {
+                                    required: true,
+                                    id: 4,
+                                    title: {
+                                        len: 20
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                },
+                code: adUnitCode,
+                bids: [
+                    {bidder: 'testBidder', params: {}}
+                ]
+            }]
+
+            pbjs.que.push(function () {
+
+                /**
+                 * BID RESPONSE SIMULATION SECTION START
+                 *
+                 * This section handles simulating a bidder
+                 * that will always respond with bid responses.
+                 *
+                 * This part should not be present in production.
+                 */
+                pbjs.registerBidAdapter(null, 'testBidder', {
+                    supportedMediaTypes: ['banner', 'video', 'native'],
+                    isBidRequestValid: () => true
+                });
+
+                pbjs.setConfig({
+                    debugging: {
+                        enabled: true,
+                        intercept: [
+                            {
+                                when: {
+                                    bidder: 'testBidder',
+                                },
+                                then: {
+                                    creativeId: 'testCreativeId',
+                                }
+                            }
+                        ]
+                    }
+                });
+pbjs.setConfig({'debug':true});
+                /**
+                 * BID RESPONSE SIMULATION SECTION END
+                 */
+
+                pbjs.addAdUnits(adUnits);
+                pbjs.requestBids({
+                    adUnitCodes: [adUnitCode],
+                    bidsBackHandler: function() {
+                        const bids = pbjs.getHighestCpmBids(adUnitCode);
+console.log(bids);
+                        const bid = bids[0];
+                        const slot = document.getElementById('native');
+                        const iframe = document.createElement('iframe');
+                        Object.entries({
+                            frameBorder: 0,
+                            marginWidth: 0,
+                            marginHeight: 0,
+                            width: 600,
+                            height: 500,
+                            scrolling: 'no',
+                            srcdoc: document.getElementById('native-template').innerHTML
+                        }).forEach(([prop, val]) => iframe.setAttribute(prop, val));
+                        slot.appendChild(iframe);
+console.log('adUnitCode: ', adUnitCode);
+console.log('bids:', bids);
+console.log('bid: ', bid);
+console.log('bid.adId: ', bid.adId);
+console.log(iframe.contentDocument);
+                        iframe.onload = () => pbjs.renderAd(iframe.contentDocument, bid.adId);
+                    }
+                });
+            });
+")
+      )
+     (:body
+      #-NIL
+      "        <template id='native-template'>
+            <style>
+                body {
+                    display: inline-block;
+                }
+        
+                .container {
+                    display: inline-block;
+                    font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;
+                    font-size: 14px;
+                    line-height: 1.42857143;
+                    box-sizing: border-box;
+                    border: 3px dashed red;
+                    padding: 20px;
+                    color: #333;
+                }
+
+                .img {
+                    width: 300px;
+                    height: 200px;
+                }
+    
+            </style>
+            <div class='container'>
+                <h1>##hb_native_asset_id_4##</h1>
+                <h3>Rating: ##hb_native_asset_id_3##</h3>
+                <img class='img' src='##hb_native_asset_id_1##' alt='bg' />
+            </div>
+        </template>
+"
+      #+NIL
+      (:template
+       :id "native-template"
+       (:style (cl-who:str (cl-css:css '((body
+					  :display inline-block)
+					 (.container
+					  :display inline-block
+					  :font-family :Helvetica
+					  :font-size 14px
+					  :line-height 1.42857143
+					  :box-sizing border-box
+					  :border "3px dashed red"
+					  :padding 20px
+					  :color \#333
+					  )
+					 (.img
+					  :width 300px
+					  :height 200px)))))
+       (:div
+	:class "container"
+	(:h1 "##hb_native_asset_id_4##")
+	(:h3 "Rating: ##hb_native_asset_id_3##")
+	(:img :class "img" :src "##hb_native_asset_id_1##" :alt "bg")))
+#|
+      (:h2 "Prebid Test Bidder Example")
+      (:h5 "Native ad")
+      (:div :id "native")       
+|#
+"
+        <h2>Prebid Test Bidder Example</h2>
+        <h5>Native ad</h5>
+        <div id='native'></div>
+    <script src='//localhost:35729/livereload.js?snipver=1' async='' defer=''></script>
+"
+      )
+     )
+    )
+  )
+
+;;
 ;;
 ;;
 
@@ -130,9 +418,9 @@
       )
      (:body
       (:div
-       :id="div-1" :style "min-height:250px"
+       :id "div-1" :style "min-height:250px"
        (:a "licensed div-1"))
       (:div
-       :id="div-2" :style "min-height:250px"
+       :id "div-2" :style "min-height:250px"
        (:a "licensed div-2"))))))
 
