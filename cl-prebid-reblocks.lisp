efc20187dc0da380eaf21b57609dbe487ae2ff6d
John Morrison <jm@symbolic-simulation.com>
2025-11-08 08:23:09 -0500

    Checkpoint.  Gotten both reblocks-lass and tailwind for styling. Going to run more experiments.

--- cl-prebid-reblocks.lisp
+++ cl-prebid-reblocks.lisp
@@ -8,7 +8,12 @@
 (in-package #:cl-prebid/reblocks)
 
 ;;
-;; If I don't define THIS method, then the CDN version of tailwind will be fetched!
+;; Kludge to prevent dependency on CDN-delivered tailwind.
+;;
+;; The reblocks ui2 widget primary get-dependencies method fetches the
+;; CDN version of tailwind.  So, over-ride it here.  Probably would be
+;; more responsible to make an :around method instead, and prune the
+;; CDN dependency from those returned by call-next-method
 ;;
 
 (defmethod reblocks-ui2/widget:get-dependencies ((widget reblocks-ui2/widget:ui-widget) (theme reblocks-ui2/themes/tailwind:tailwind-theme))
@@ -24,8 +29,13 @@
    (call-next-method)))
 
 ;;
-;; This binds the STRING values of the various HTML parameters (in lowercase)
+;; This macro binds the STRING values of the various HTML parameters (in lowercase).  It is useful 
 ;;
+;;  Use this to bind string values of request params foo, baz, and frob, respectively
+;;
+;; (reblocks-request-params (foo baz frob)
+;;    <body referencing foo, baz, and frob as bound vars> )
+;; 
 
 (defmacro reblocks-request-params (lambda-list &body body)
   (let ((temp-var (gensym)))
@@ -35,6 +45,38 @@
 		   for i from 0
 		   collect `(,var (aget ,temp-var ,(string-downcase (symbol-name var)))))
 	 ,@body))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+;;;;; A string widget, styled with tailwind
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defwidget a-string-widget (ui-widget)
+  ((content :accessor content :initarg :content :initform "")))
+
+(defmethod render ((widget a-string-widget) (theme reblocks-ui2/themes/tailwind:tailwind-theme))
+  (reblocks-request-params (foo baz frob)
+		     (with-html ()
+		       (:div :class "text-2xl my-8" ; Reference tailwind style(s)
+			     (:p (content widget))
+			     (:p (format nil "foo ~s" foo))
+			     (:p (format nil "baz ~s" baz))
+			     (:p (format nil "looking up frob ~s" frob))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+;;;; A prebid container widget (page wrap, really)
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;
 ;; (reblocks/preview:preview (make-instance 'todo::cl-prebid-container))
@@ -53,19 +95,30 @@
 (defmethod reblocks-ui2/widget:render ((cpc cl-prebid-container) (theme reblocks-ui2/themes/tailwind:tailwind-theme))
   #+NIL (clouseau:inspect (list :reblocks-ui2/widget-render cpc))
   (with-html ()
-    (:div :id "banner"
-	  :class "banner"
-	  (:h1 :class "text-2xl my-8" "cbc h1")
-	  (:a "banner text"))
-    (:div :class "text-8xl text-gray-500 dar:text-gray-400" (:a "FOOO"))
-    (:div :content
-	  (reblocks-ui2/widget:render (content cpc) theme))
+
+    ;; These are styled with our locally-built, enormously
+    ;; over-inclusive tailwind css file
+
+    (:div :id "tailwind00" :class "mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	  (:a "tailwind00 text"))
+    (:div :id "header" (:h1 :class "text-2xl my-8" (:a "header text")))
+    (:div :id "foo" :class "text-8xl text-gray-500 dar:text-gray-400" (:a "FOOO"))
+
+    ;; These are styled using the reblocks-lass defined in the following get-dependencies method
+
+    (:div :id "banner" :class "banner" (:a "banner text"))
     (:div :id "bodystuff"
 	  :class "bodystuff"
 	  (:a "bodystuff text")
 	  (loop for item in (reblocks-ui2/containers/container:subwidgets cpc) do
 		#+NIL (clouseau:inspect item)
-		(reblocks-ui2/widget:render item theme)))))
+		(reblocks-ui2/widget:render item theme)))
+
+    ;; Here we render the contents of this container
+
+    (:div :content (reblocks-ui2/widget:render (content cpc) theme))
+    )
+  )
 
 ;; https://40ants.com/reblocks/dependencies/#x-28REBLOCKS-2FDOC-2FDEPENDENCIES-3A-3A-40DEPENDENCIES-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29
 
@@ -99,7 +152,15 @@
     )
    (call-next-method))) ; Otherwise a remote CDN dependency upon tailwind
 
-;;;;;; Application
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+;;;; Application
+;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 #-NIL
 (defun wrap-with-frame (widget)
@@ -111,19 +172,6 @@
 ;;;; Pages
 ;;;;
 
-
-(defwidget a-string-widget (ui-widget)
-  ((content :accessor content :initarg :content :initform "")))
-
-(defmethod render ((widget a-string-widget) (theme reblocks-ui2/themes/tailwind:tailwind-theme))
-  (reblocks-request-params (foo baz frob)
-		     (with-html ()
-		       (:div :class "text-2xl my-8" ; Reference tailwind style(s)
-			     (:p (content widget))
-			     (:p (format nil "foo ~s" foo))
-			     (:p (format nil "baz ~s" baz))
-			     (:p (format nil "looking up frob ~s" frob))))))
-
 ;;;;
 ;;;; App
 ;;;;
@@ -134,6 +182,17 @@
     :routes ((page ("/" :name "root") (make-instance 'a-string-widget :content "ROOT"))
 	     (page ("/one" :name "foo") (make-instance 'a-string-widget :content "ONE"))
 	     (page ("/two" :name "two") (make-instance 'a-string-widget :content "TWO"))))
+
+(defmethod reblocks/dependencies:get-dependencies ((app my-app))
+  (clouseau:inspect app)
+  (list*
+   (reblocks/dependencies:make-dependency
+     "https://cdn.tailwindcss.com/3.3.6"
+     ;; Old URLs:
+     ;; "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"
+     ;; "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"
+     :type :js)
+   (call-next-method)))
 
 ;;;;
 ;;;; Plumbing
